6705a8bc8faa6c92f6bce24b8665501cb8b7a741
Realzwen <zzwen671@gmail.com>
2026-02-06 08:50:56 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+title Discord Sunucu sikici
+color 0A
+echo.
+echo ================================
+echo   Discord Sunucu sikici
+echo ================================
+echo.
+node index.js
+pause
